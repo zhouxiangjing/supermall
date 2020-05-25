fbb2926e6c420b94f1b707ea2005005c3689fd8a
zhouxiangjing <zhouxiangjing2018@163.com>
2020-05-26 00:16:49 +0800

    add tab bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
+<script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+
+export default {
+  name: "App",
+  components: {
+    MainTabBar
+  },
+  methods: {
+    test() {
+      console.log("zzzzz");
+    }
+  }
+};
+</script>
+
 <style>
-@import 'assets/css/base.css'
+@import "assets/css/base.css";
 </style>
